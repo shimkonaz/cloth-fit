8e89d7e339261069d98268c312c8d161cc08cbbf
unknown <nazarkoo4435@gmail.com>
2018-11-14 18:32:56 +0200

    refactored table update behaviour

--- src/size.js
+++ src/size.js
@@ -1,37 +1,12 @@
 export const getSize = () => {
-
+  
+  const res = [];
   const arr = document.getElementsByClassName('slider-output');
   const tbody = document.getElementById('results-table').getElementsByTagName('tbody')[0];
-  const res = [];
 
   for (const elem of arr) {
-    const parsedNum = parseInt(elem.innerHTML);
-    const parsedS = parsedNum >= 50 && parsedNum < 100;
-    const parsedM = parsedNum > 100 && parsedNum < 150;
-    const parsedL = parsedNum > 150 && parsedNum < 200;
-    const parsedXL = parsedNum > 200 && parsedNum < 246;
-
-    if ((elem.id === 'chest') && parsedS) {
-      res.push({
-        type: 'Shirt',
-        size: 'S'
-      });
-    } else if ((elem.id === 'chest') && parsedM) {
-      res.push({
-        type: 'Shirt',
-        size: 'M'
-      });
-    } else if ((elem.id === 'chest') && parsedL) {
-      res.push({
-        type: 'Shirt',
-        size: 'L'
-      });
-    } else if ((elem.id === 'chest') && parsedXL) {
-      res.push({
-        type: 'Shirt',
-        size: 'XL'
-      });
-    }
+    switchMeasures(elem, res);
+    
   }
 
   for (const row of res) {
@@ -39,10 +14,77 @@ export const getSize = () => {
   }
 };
 
+const getSizeValue = v => {
+  let res;
+
+  if (v >= 50 && v < 100) {
+    res = 'S';
+  } else if (v > 100 && v < 150) {
+    res = 'M';
+  } else if (v > 150 && v < 200) {
+    res = 'L';
+  } else if (v > 200 && v < 246) {
+    res = 'XL';
+  } else {
+    res = '';
+  }
+
+  return res;
+};
+
+const switchMeasures = (elem, res) => {
+  const v = parseInt(elem.innerHTML);
+
+  switch (elem.id) {
+    case 'chest':
+      res.push({
+        type: 'Shirt',
+        size: getSizeValue(v)
+      });
+      break;
+
+    case 'waist':
+      res.push({
+        type: 'Shirt',
+        size: getSizeValue(v)
+      });
+      break;
+
+    case 'hip':
+      res.push({
+        type: 'Shirt',
+        size: getSizeValue(v)
+      });
+      break;
+
+    case 'height':
+      res.push({
+        type: 'Smoking',
+        size: getSizeValue(v)
+      });
+      break;
+
+    case 'sleeve':
+      res.push({
+        type: 'T-Shirt',
+        size: getSizeValue(v)
+      });
+      break;
+
+    case 'inseam':
+      res.push({
+        type: 'Trousers',
+        size: getSizeValue(v)
+      });
+  }
+};
+
 const createRow = (table, type, size) => {
   const row = table.insertRow(-1);
+  row.className = 'row';
   const cell1 = row.insertCell(0);
   const cell2 = row.insertCell(1);
+  cell1.className = 'type';
   cell1.innerHTML = type;
   cell2.innerHTML = size;
 };
